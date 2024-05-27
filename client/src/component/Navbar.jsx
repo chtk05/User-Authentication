98ffaa7a29c5f8d814044ff071566cd9db279e32
Chattarika Bunchongpru <saiparn@Chattarikas-MacBook-Air.local>
2024-05-28 03:50:12 +0700

    navbar register and login page plus backend bug fix

---
+++ client/src/component/Navbar.jsx
@@ -0,0 +1,42 @@
+import { Link, NavLink } from "react-router-dom";
+import { useAuth } from "../provider/AuthProvider";
+const Navbar = () => {
+  const { isLoggedIn, logout } = useAuth();
+  return (
+    <nav className="flex justify-between shadow-xl box-border bg-[#01204E] py-2">
+      <div to={"/"} className="px-6 w-full mx-auto flex flex-row gap-2">
+        <p className="mt-2 text-2xl font-bold text-[#FFA62F]">Anime Club</p>
+      </div>
+      <div>
+        {isLoggedIn ? (
+          <div className="flex flex-row gap-4  font-semibold px-2 mt-1">
+            <NavLink
+              to="/profile"
+              className="bg-[#DFF6FF] text-[#1363DF] p-2 border rounded-lg hover:bg-[#47B5FF] hover:text-[#DFF6FF]"
+            >
+              Profile
+            </NavLink>
+            <p
+              onClick={logout}
+              className="bg-[#DFF6FF] text-[#1363DF] p-2 border rounded-lg hover:bg-[#47B5FF] hover:text-[#DFF6FF]"
+            >
+              Logout
+            </p>
+          </div>
+        ) : (
+          <div className="flex flex-row gap-4  font-semibold px-2 mt-1">
+            {" "}
+            <NavLink
+              to="/login"
+              className="bg-[#DFF6FF] text-[#1363DF] p-2 border rounded-lg hover:bg-[#47B5FF] hover:text-[#DFF6FF]"
+            >
+              Login
+            </NavLink>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
